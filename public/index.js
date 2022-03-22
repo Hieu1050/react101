4a2c0946bb3142ea6059f69bcfa260888a13f538
Hieu1050 <vuchihieu1050@gmail.com>
2022-03-22 15:30:03 +0900

    first styling

--- public/index.js
+++ public/index.js
@@ -1,46 +1,52 @@
 function Header (){
-   return (
-      <header>
-            <nav>
-               <img src="./logo192.png"/>
-            </nav>
-      </header>
-   )
+  return (
+    <header>
+          <nav className = "nav-bar">
+              <img className = "nav-logo" src="./logo192.png" />
+          <ul className = "nav-items">
+              <li>Trust</li>
+              <li>The</li>
+              <li>Process</li>
+          </ul>
+          </nav>
+
+    </header>
+  )
 }
 function Content (){
-   return (
-      <div>
-         <h1>Why I learn react</h1>
-         <ol>
-               <li>
-                  Because I only know back-end
-               </li>
-               <li>
-                  Because I want to make my own web project
-               </li>
-               <li>
-                  Because I want to have a job so bad
-               </li>
-         </ol>
-      </div>
-   )
+  return (
+    <div>
+        <h1>Why I learn react</h1>
+        <ol>
+              <li>
+                Because I only know back-end
+              </li>
+              <li>
+                Because I want to make my own web project
+              </li>
+              <li>
+                Because I want to make my own web project, again
+              </li>
+        </ol>
+    </div>
+  )
 }
 function Footer (){
-   return (
-         <footer>
-            <small>Written by hieuchivu on 2022-03-22</small>
-         </footer>
-   )
+  return (
+    <footer>
+      <small>Written by hieuchivu on 2022-03-22</small>
+    </footer>
+  )
 }
 
 function Page (){
-   return (
-      <div>
-         <Header />
-         <Content />
-         <Footer />
-      </div>
-   )
+  return (
+  <div>
+    <Header />
+    <Content />
+    <Footer />
+  </div>
+  )
 }
 
 ReactDOM.render (
