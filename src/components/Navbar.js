fbdaea50bf9b2348ecb7396446fd083aae62f8f1
Hieu1050 <vuchihieu1050@gmail.com>
2022-03-23 16:10:52 +0900

    navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,11 @@
+import reactLogo192 from "../images/logo192.png"
 function Navbar (){
     return (
-        <h1>
-            Navbar goes here
-        </h1>
+        <nav className = "nav-bar">
+            <img className = "nav-logo" src= {reactLogo192} alt="reactLogo192" />
+            <h3 className = "">ReactFacts</h3>
+            <h4>React Course - Project 1</h4>
+        </nav>
     )
 }
 export default Navbar
