e1f528c238890efae25dddd1eef9ed2d1a3306dd
Hieu1050 <vuchihieu1050@gmail.com>
2022-03-23 16:35:49 +0900

    bullets styling

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,7 +4,7 @@ function Main (){
             <h1>
             Fun facts about React
             </h1>
-        <ul>
+        <ul className = "main--facts">
             <li>Was first released in 2013</li>
             <li>Was originally created by Jordan Walke</li>
             <li>Has well over 100l stars on GitHub</li>
