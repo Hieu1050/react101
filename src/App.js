9298f9b443060fb1fcd2e9058f32800dc69cb51a
Hieu1050 <vuchihieu1050@gmail.com>
2022-03-29 10:38:42 +0900

    mapping components: props as spreading data object

--- src/App.js
+++ src/App.js
@@ -5,15 +5,11 @@ import Hero from './components/Hero'
 import Card from './components/Card'
 import cardData from './data'
 function App() {
+    // mapping each data set to a Card component, return that list of Card components
     const cardElements = cardData.map (item => {
         return (<Card 
-            card_img = {item.coverImg}
-            card_star = "card-star.png"
-            card_rating = {item.stats.rating}
-            card_popularity = {item.stats.reviewCount}
-            card_location = {item.location}
-            card_title = {item.title}
-            card_pricing = {item.price}
+            key = {item.id}
+            {...item}
         />)
     })
     
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,17 +1,25 @@
 import React from "react"
 export default function Card (props){
+    let badgeText
+    if (props.openSpots === 0) {
+        badgeText = "SOLD OUT"
+    } else if (props.location === "Online") {
+        badgeText = "ONLINE"
+    }
+    
     return (
         <div className = "card">
-            
-            <img className = "card-image" alt = "card image" src = {`/images/${props.card_img}`} />
+            {/* if badgeText is NOT null, display its content */}
+            {badgeText && <div className="card-badge">{badgeText} </div>}
+            <img className = "card-image" alt = "card image" src = {`/images/${props.coverImg}`} />
             <div className = "card-rating-loc">
-                <img className = "card-star" alt = "star" src = {`images/${props.card_star}`} />
-                <span className = "card-rating"> {props.card_rating}</span>
-                <span  className="gray"> ({props.card_popularity}) • </span>
-                <span  className="gray"> {props.card_location}</span>
+                <img className = "card-star" alt = "star" src = "/images/card-star.png" />
+                <span className = "card-rating"> {props.stats.rating}</span>
+                <span  className="gray"> ({props.stats.reviewCount}) • </span>
+                <span  className="gray"> {props.location}</span>
             </div>
-            <p className = "card-description"> {props.card_title}</p>
-            <p className = "card-pricing"> <span className="bold">From {props.card_pricing}$</span> / person</p>
+            <p className = "card-description"> {props.title}</p>
+            <p className = "card-pricing"> <span className="bold">From {props.price}$</span> / person</p>
         </div>
     )
 }
